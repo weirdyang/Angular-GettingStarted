2a9019f4294b69eb52fc14d14d178bebdabdae08
weirdyang <wy4786@gmail.com>
2021-05-19 13:18:38 +0800

    module 11 complete

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -6,17 +6,45 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        component: ProductListComponent,
+      },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent
+      },
+      {
+        path: 'welcome',
+        component: WelcomeComponent
+      },
+      {
+        path: '',
+        redirectTo: 'welcom',
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: 'welcome'
+      }
+    ]),
   ],
   bootstrap: [AppComponent]
 })
